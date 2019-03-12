a21ef470f4a34d86582dcd816c80e1c5e86725d9
bassiebangura <devbassiebangura@gmail.com>
2019-03-12 09:34:28 -0400

    Stable form of Mobile Users Chart

--- js/main.js
+++ js/main.js
@@ -1,39 +1,25 @@
-console.log("Hello Brother!")
-alert("Hello Brother!!!")
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
+/**************************************************************
+                     MOBILE USESER CHART
+***************************************************************/
+let usersDeviceTypeLabels = ["Phone", "Tablets", "Desktop"]
+let usersDeviceTypeData =[5, 10, 25]
+let mobileUsersChartBackgroundColor =  ['rgba(255, 0, 0)', 'rgba(0, 255, 0)', 'rgba(0, 0, 255)']
+let mobileUsersChartCanvas = document.getElementById("mobile-users-chart__figure").getContext('2d');
+let mobileUsersChartConfig = {
+    type: 'doughnut',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: usersDeviceTypeLabels,
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            data: usersDeviceTypeData,
+            backgroundColor: mobileUsersChartBackgroundColor
         }]
     },
     options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
+        legend: {
+            position: 'right'
         }
     }
-});
+}
+
+/*** Create new instance of chart using mobile users canvas and mobile users config object  ***/
+let mobileUsersChart = new Chart(mobileUsersChartCanvas, mobileUsersChartConfig);
